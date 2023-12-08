a5abd0991e8428bdffe32ee176d81752ccaed403
Walocial <Walocial@hotmail.com>
2023-12-08 10:03:08 +0100

    Data fra api

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,3 +1,14 @@
+import { displayData, displaySpecificEvent, displaySpecificTypeOfEvents } from "./api-service.js";
 import { changeLanguage } from "./language.js";
+
+document.addEventListener('DOMContentLoaded', function() {
+    displaySpecificTypeOfEvents('kurser');
+});
+
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+setTheme('darkMode');
 
 changeLanguage('da', 'index');
