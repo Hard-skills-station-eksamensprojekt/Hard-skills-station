b08804c98494deac75dd49a767fa0a7950a847b7
Walocial <Walocial@hotmail.com>
2023-12-10 20:12:16 +0100

    PC bugfixes to the settings display
    -Resolved bug of course price overlapping settings menu.
    -Made settings toggleable with the cog icon

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,10 +1,11 @@
 import { displayData, displaySpecificEvent, displaySpecificTypeOfEvents } from "./api-service.js";
 import { changeLanguage } from "./language.js";
 
+//Henter data fra databasen gennem API:
 document.addEventListener('DOMContentLoaded', function() {
     displaySpecificTypeOfEvents('kurser');
 });
-
+//Ændrer temaet på hjemmesiden når der klikkes på slider knappen:
 const themeSwitch = document.getElementById('themeSwitch');
 themeSwitch.addEventListener('click', function() {
     if(themeSwitch.checked) {
@@ -15,14 +16,27 @@ themeSwitch.addEventListener('click', function() {
         document.documentElement.className = 'default';
     }
 });
-
-const savedTheme = localStorage.getItem('theme');
+//Checker om et tema er gemt i localstorage, og displayer det hvis der er:
+const savedTheme = localStorage.getItem('theme'); 
 if (savedTheme) {
     document.documentElement.className = savedTheme;
     if (savedTheme === 'darkMode') {
         themeSwitch.checked;
     }
 }
+//Skjuler eller viser indstillinger når der trykkes på tandhjul-ikonet
+const cog = document.getElementById('settingsCog');
+const settings = document.getElementById('settings');
+let isOpen = false;
+cog.addEventListener('click', function() {
+    isOpen = !isOpen;
+    if (isOpen) {
+        settings.style.top = '5em';
+    } else {
+        settings.style.top = '-10em';
+    }
+});
+
 
 
 changeLanguage('da', 'index');
